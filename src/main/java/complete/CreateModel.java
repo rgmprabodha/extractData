0923ec8f46e615478e1d584ec16d56263a8b178e
rgmprabodha <rgmprabodha@gmail.com>
2019-12-05 00:32:56 +0100

    Complete folder

--- src/main/java/complete/CreateModel.java
+++ src/main/java/complete/CreateModel.java
@@ -1,6 +1,7 @@
 package complete;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -17,6 +18,8 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.jena.query.DatasetAccessor;
+import org.apache.jena.query.DatasetAccessorFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
@@ -29,6 +32,7 @@ import org.json.JSONObject;
 public class CreateModel {
 	static Model model;
 	static String staticDBFile = "E:\\CPS2\\Year_2\\Semantic_Web\\Jena\\extractdata\\src\\main\\java\\extractdata\\static.ttl";
+	private static final String FUESKI_LOCAL_ENDPOINT = "http://localhost:3030/bicycle"; // I have two databases; classes and instances, so put two values, add three 
 	static String stationURIPrefix;
 	static String[] cities = {"SAINT-ETIENNE", "LYON", "PARIS"};
 	static String exNS;
@@ -38,19 +42,25 @@ public class CreateModel {
 		CreateModel.sslResolve();
 		CreateModel.initializeModel();
 		for (String city : cities) {
+			List<Station> stations = null;
 			if(city == "SAINT-ETIENNE") {
 				StaticSaintEtienne se = new StaticSaintEtienne();
-				List<Station> stations = se.processData();
+				stations = se.processData();
 				addCityToModel(stations);
-			}else if(city == "Lyon") {
-				
 			}
-		}
-		try {
-			model.write(new FileOutputStream(new File(staticDBFile)), "TURTLE");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			else  if(city == "LYON") {
+				StaticLyon ly = new StaticLyon();
+				stations = ly.processData();
+				addCityToModel(stations);
+			}
+			else  if(city == "PARIS") {
+				StaticParis pa = new StaticParis();
+				stations = pa.processData();
+				addCityToModel(stations);
+			}
+			
+			// Save the model in fueski server
+			saveToFueski();
 		}
 	}
 	
@@ -83,16 +93,13 @@ public class CreateModel {
 		model = ModelFactory.createDefaultModel();
 		exNS = "http://www.example.com/";
 		geoNS = "https://www.w3.org/2003/01/geo/wgs84_pos#";
-
 		model.setNsPrefix("ex", exNS);
 		model.setNsPrefix("geo", geoNS);
-
 		stationURIPrefix = exNS + "Station:";
 	}
 	
 	public static void addCityToModel(List<Station> stations) {
 		for (Station station : stations) {	
-
 			Resource StationRs = model.createResource(stationURIPrefix + station.getID());
 			StationRs.addProperty(RDF.type, RDFS.Class); // TODO station instance should be type of station class
 			StationRs.addProperty(FOAF.name, station.getName());
@@ -100,6 +107,28 @@ public class CreateModel {
 			StationRs.addLiteral(model.createProperty(geoNS + "lat"), station.getLat());
 			StationRs.addLiteral(model.createProperty(geoNS + "long"), station.getLon());
         }
+	}
+	
+	/**
+	 * what is the best way?
+	 * 1. save to a file and put it to server
+	 * 2. directly put it to server
+	 */
+	public static void saveToFueski() {
+		try {
+			model.write(new FileOutputStream(new File(staticDBFile)), "TURTLE");
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Model m = ModelFactory.createDefaultModel();
+		try (FileInputStream in = new FileInputStream(staticDBFile)) {
+			m.read(in, null, "TURTLE");			
+			DatasetAccessor accessor = DatasetAccessorFactory.createHTTP(FUESKI_LOCAL_ENDPOINT);
+			accessor.putModel(m);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 }
